822e3a107f32468abaded7b97776cface490fa12
Tsukasa Hamano <hamano@osstech.co.jp>
2010-05-14 17:24:31 +0900

    added Context test

---
+++ tests/settings.py
@@ -0,0 +1,8 @@
+# This is setting template.
+# You must change these parameter if you perform test.
+
+SERVER = "server"
+SHARE = "share"
+USERNAME = "username"
+PASSWORD = "password"
+
---
+++ tests/test_context.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import unittest
+import smbc
+import settings
+
+class TestContext(unittest.TestCase):
+    def setUp(self):
+        self.ctx = smbc.Context()
+
+    def tearDown(self):
+        del self.ctx
+
+    def testWorkgroup(self):
+        list = self.ctx.opendir('smb://').getdents()
+        assert(len(list) > 0)
+        for entry in list:
+            assert(entry.smbc_type == 1)
+
+    def testServer(self):
+        list = self.ctx.opendir('smb:///').getdents()
+        assert(len(list) > 0)
+        for entry in list:
+            assert(entry.smbc_type == 2)
+
+    def testShare(self):
+        uri = 'smb://' + settings.SERVER
+        list = self.ctx.opendir(uri).getdents()
+        assert(len(list) > 0)
+        for entry in list:
+            assert(entry.smbc_type == 3 or entry.smbc_type == 6)
+
+if __name__ == '__main__':
+    unittest.main()
