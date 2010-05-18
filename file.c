e130a759dde8ce658a8a3a4984f8f3a48c30815d
Tsukasa Hamano <hamano@osstech.co.jp>
2010-05-18 14:33:03 +0900

    added Author

--- file.c
+++ file.c
@@ -3,6 +3,7 @@
  * Copyright (C) 2002, 2005, 2006, 2007, 2008, 2010  Red Hat, Inc
  * Authors:
  *  Tim Waugh <twaugh@redhat.com>
+ *  Tsukasa Hamano <hamano@osstech.co.jp>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
