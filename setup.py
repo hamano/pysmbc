fbb045203833288c83c5863a0cf4eb73b32417a5
Tim Waugh <twaugh@redhat.com>
2010-05-20 12:57:07 +0100

    Added license boilerplate and long_description to setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,45 @@
+#!/usr/bin/env python
+
+## Copyright (C) 2002, 2005, 2006, 2007, 2008, 2010  Red Hat, Inc
+## Authors:
+##  Tim Waugh <twaugh@redhat.com>
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+"""This is a set of Python bindings for the libsmbclient library
+from the samba project.
+
+Example:
+
+>>> import smbc
+>>> ctx = smbc.Context (auth_fn=my_auth_callback_fn)
+>>> entries = ctx.opendir ("smb://SERVER").getdents ()
+>>> for entry in entries:
+...     print entry
+<smbc.Dirent object "music" (File share) at 0x7fbd7c42b3a0>
+<smbc.Dirent object "IPC$" (IPC share) at 0x7fbd7c42b148>
+<smbc.Dirent object "Charlie" (Printer share) at 0x7fbd7c42b3c8>
+>>> d = ctx.open ("smb://SERVER/music")
+etc.
+"""
+
 from distutils.core import setup, Extension
 setup (name="pysmbc",
        version="1.0.7",
        description="Python bindings for libsmbclient",
+       long_description=__doc__,
        author="Tim Waugh",
        author_email="twaugh@redhat.com",
        maintainer="Tim Waugh",
