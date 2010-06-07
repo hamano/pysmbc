07c3241ab6d75b3356083b8b26010cf7782751b3
Tsukasa Hamano <hamano@osstech.co.jp>
2010-06-07 22:19:08 +0900

    tagged 1.0.9_rc3

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ hello
 
 from distutils.core import setup, Extension
 setup (name="pysmbc",
-       version="1.0.9_rc2",
+       version="1.0.9_rc3",
        description="Python bindings for libsmbclient",
        long_description=__doc__,
        author=["Tim Waugh <twaugh@redhat.com>",
