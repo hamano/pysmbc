2e860cd2a0bbf4af1066fb9e2a622bd89c8ff105
Tim Waugh <twaugh@redhat.com>
2010-11-01 16:20:08 +0000

    Bumped version for release.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ hello
 
 from distutils.core import setup, Extension
 setup (name="pysmbc",
-       version="1.0.9.1",
+       version="1.0.10",
        description="Python bindings for libsmbclient",
        long_description=__doc__,
        author=["Tim Waugh <twaugh@redhat.com>",
