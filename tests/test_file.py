5d8e8ca242c955d6294078f3073019ab5c7dd8cb
Tsukasa Hamano <hamano@osstech.co.jp>
2011-07-01 15:17:08 +0900

    added file copy operation test(buffering, iteration)

--- tests/test_file.py
+++ tests/test_file.py
@@ -26,7 +26,26 @@ def md(path):
     file.close()
     return m.hexdigest()
 
-def upload(spath, ctx, duri):
+def upload_simple(spath, ctx, duri):
+    sfile = open(spath, 'rb')
+    dfile = ctx.open(duri, os.O_CREAT | os.O_TRUNC | os.O_WRONLY)
+    ret = dfile.write(sfile.read())
+    if ret < 0:
+        raise IOError("smbc write error")
+    sfile.close()
+    dfile.close()
+    return True
+
+def download_simple(ctx, suri, dpath):
+    sfile = ctx.open(suri, os.O_RDONLY)
+    dfile = open(dpath, 'wb')
+    dfile.write(sfile.read())
+    dfile.flush()
+    sfile.close()
+    dfile.close()
+    return True
+
+def upload_buffer(spath, ctx, duri):
     sfile = open(spath, 'rb')
     dfile = ctx.open(duri, os.O_CREAT | os.O_TRUNC | os.O_WRONLY)
     while True:
@@ -40,13 +59,34 @@ def upload(spath, ctx, duri):
     dfile.close()
     return True
 
-def download(ctx, suri, dpath):
+def download_buffer(ctx, suri, dpath):
     sfile = ctx.open(suri, os.O_RDONLY)
     dfile = open(dpath, 'wb')
     while True:
         buf = sfile.read(8192)
         if not buf:
             break
+        dfile.write(buf)
+    dfile.flush()
+    sfile.close()
+    dfile.close()
+    return True
+
+def upload_iter(spath, ctx, duri):
+    sfile = open(spath, 'rb')
+    dfile = ctx.open(duri, os.O_CREAT | os.O_TRUNC | os.O_WRONLY)
+    for buf in sfile:
+        ret = dfile.write(buf)
+        if ret < 0:
+            raise IOError("smbc write error")
+    sfile.close()
+    dfile.close()
+    return True
+
+def download_iter(ctx, suri, dpath):
+    sfile = ctx.open(suri, os.O_RDONLY)
+    dfile = open(dpath, 'wb')
+    for buf in sfile:
         dfile.write(buf)
     dfile.flush()
     sfile.close()
@@ -63,23 +103,65 @@ def tearDown():
     global ctx
     del ctx
 
-def test_FileCopy1k():
+def test_copy_simple_1k():
     creat_randfile('test1.dat', 1000)
     testfile = basedir + '/' + 'test1.dat'
-    upload('test1.dat', ctx, testfile)
-    download(ctx, testfile, 'test2.dat')
+    upload_simple('test1.dat', ctx, testfile)
+    download_simple(ctx, testfile, 'test2.dat')
     assert(md('test1.dat') == md('test2.dat'))
 
-def test_FileCopy100k():
+def test_copy_simple_100k():
     creat_randfile('test1.dat', 10000)
     testfile = basedir + '/' + 'test1.dat'
-    upload('test1.dat', ctx, testfile)
-    download(ctx, testfile, 'test2.dat')
+    upload_simple('test1.dat', ctx, testfile)
+    download_simple(ctx, testfile, 'test2.dat')
     assert(md('test1.dat') == md('test2.dat'))
 
-def test_FileCopy1M():
+def test_copy_simple_1M():
     creat_randfile('test1.dat', 1000000)
     testfile = basedir + '/' + 'test1.dat'
-    upload('test1.dat', ctx, testfile)
-    download(ctx, testfile, 'test2.dat')
+    upload_simple('test1.dat', ctx, testfile)
+    download_simple(ctx, testfile, 'test2.dat')
+    assert(md('test1.dat') == md('test2.dat'))
+
+def test_copy_buffer_1k():
+    creat_randfile('test1.dat', 1000)
+    testfile = basedir + '/' + 'test1.dat'
+    upload_buffer('test1.dat', ctx, testfile)
+    download_buffer(ctx, testfile, 'test2.dat')
+    assert(md('test1.dat') == md('test2.dat'))
+
+def test_copy_buffer_100k():
+    creat_randfile('test1.dat', 10000)
+    testfile = basedir + '/' + 'test1.dat'
+    upload_buffer('test1.dat', ctx, testfile)
+    download_buffer(ctx, testfile, 'test2.dat')
+    assert(md('test1.dat') == md('test2.dat'))
+
+def test_copy_buffer_1M():
+    creat_randfile('test1.dat', 1000000)
+    testfile = basedir + '/' + 'test1.dat'
+    upload_buffer('test1.dat', ctx, testfile)
+    download_buffer(ctx, testfile, 'test2.dat')
+    assert(md('test1.dat') == md('test2.dat'))
+
+def test_copy_iter_1k():
+    creat_randfile('test1.dat', 1000)
+    testfile = basedir + '/' + 'test1.dat'
+    upload_iter('test1.dat', ctx, testfile)
+    download_iter(ctx, testfile, 'test2.dat')
+    assert(md('test1.dat') == md('test2.dat'))
+
+def test_copy_iter_100k():
+    creat_randfile('test1.dat', 10000)
+    testfile = basedir + '/' + 'test1.dat'
+    upload_iter('test1.dat', ctx, testfile)
+    download_iter(ctx, testfile, 'test2.dat')
+    assert(md('test1.dat') == md('test2.dat'))
+
+def test_copy_iter_1M():
+    creat_randfile('test1.dat', 1000000)
+    testfile = basedir + '/' + 'test1.dat'
+    upload_iter('test1.dat', ctx, testfile)
+    download_iter(ctx, testfile, 'test2.dat')
     assert(md('test1.dat') == md('test2.dat'))
