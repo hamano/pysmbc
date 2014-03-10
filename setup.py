ca6fed57eb5ae6eb413e34446f5da6d029943b14
HAMANO Tsukasa <hamano@osstech.co.jp>
2014-03-10 15:41:48 +0900

    release 1.0.14

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ def pkgconfig_I (pkg):
     return dirs
     
 setup (name="pysmbc",
-       version="1.0.13",
+       version="1.0.14",
        description="Python bindings for libsmbclient",
        long_description=__doc__,
        author=["Tim Waugh <twaugh@redhat.com>",
