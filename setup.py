247830c3711a6a99569f8a1f8941629045939425
HAMANO Tsukasa <hamano@osstech.co.jp>
2016-11-22 18:49:43 +0900

    bumpup version

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ def pkgconfig_I (pkg):
     return dirs
     
 setup (name="pysmbc",
-       version="1.0.15.5",
+       version="1.0.15.6",
        description="Python bindings for libsmbclient",
        long_description=__doc__,
        author=["Tim Waugh <twaugh@redhat.com>",
