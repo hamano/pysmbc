bfe8741faac8f255f72b7b4bca884ba6cc6abc54
HAMANO Tsukasa <hamano@osstech.co.jp>
2023-01-09 13:50:37 +0900

    release 1.0.25

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ def pkgconfig_Dversion(pkg, prefix=None):
 
 setup(
     name="pysmbc",
-    version="1.0.24",
+    version="1.0.25",
     description="Python bindings for libsmbclient",
     long_description=__doc__,
     author=[
